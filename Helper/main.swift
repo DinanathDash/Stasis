5a140556aa0f563be03b0437df0d38f2e9496f7a
DinanathDash <dashdinanath056@gmail.com>
2026-08-28 11:45:03 +0530

    refactor: dynamically generate code signing requirements using the leaf certificate's common name

--- Helper/main.swift
+++ Helper/main.swift
@@ -19,9 +19,25 @@ class ServiceDelegate: NSObject, NSXPCListenerDelegate {
             return false
         }
 
-        // Construct the requirement: identifier "com.dinanathdash.stasis" and Team ID
+        // Dynamically build requirement matching our own signing certificate's Common Name
+        var selfCode: SecCode?
+        guard SecCodeCopySelf(SecCSFlags(rawValue: 0), &selfCode) == errSecSuccess,
+              let code = selfCode else { return false }
+        
+        var dict: CFDictionary?
+        let staticCode = unsafeBitCast(code, to: SecStaticCode.self)
+        guard SecCodeCopySigningInformation(staticCode, SecCSFlags(rawValue: kSecCSSigningInformation), &dict) == errSecSuccess,
+              let info = dict as? [String: Any],
+              let certs = info[kSecCodeInfoCertificates as String] as? [SecCertificate],
+              let leaf = certs.first else { return false }
+        
+        var cnCF: CFString?
+        SecCertificateCopyCommonName(leaf, &cnCF)
+        guard let commonName = cnCF as String? else { return false }
+        
+        let reqString = "anchor apple generic and identifier \"com.dinanathdash.stasis\" and certificate leaf[subject.CN] = \"\(commonName)\"" as CFString
+        
         var requirement: SecRequirement?
-        let reqString = "anchor apple generic and identifier \"com.dinanathdash.stasis\" and (certificate leaf[subject.OU] = \"63P4XT247T\" or certificate leaf[subject.CN] = \"Apple Development: dashdinanath056@gmail.com (63P4XT247T)\")" as CFString
         let reqStatus = SecRequirementCreateWithString(reqString, [], &requirement)
         guard reqStatus == errSecSuccess, let validReq = requirement else {
             return false
